0dbbfab29c84b3bd82b140cd39d3fb578c8d5637
oracle <oracle@GBTC441R01RH.montgomerycollege.edu>
2016-06-13 11:02:51 -0400

    created basic Hello World Java Project

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println( "Hello, World!" );
+		System.out.println("Today is June 13th, 2015. I am alive!");
+		
+
+	}
+
+}
